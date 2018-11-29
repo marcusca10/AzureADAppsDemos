051837983be15a2f9538af7aac2e463206cd3b51
Marcus Carvalho <marcusca@microsoft.com>
2018-11-29 10:05:54 +0100

    VS Solution created

---
+++ WsFed/McaAadWsfed/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace McaAadWsfed.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
