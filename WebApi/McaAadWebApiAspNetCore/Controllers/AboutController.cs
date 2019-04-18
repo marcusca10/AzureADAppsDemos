7f4bc5d186ce454e49d418d06ae5b290943b1c26
Marcus Carvalho <marcusca@microsoft.com>
2019-04-18 14:40:12 -0300

    User information controller

---
+++ WebApi/McaAadWebApiAspNetCore/Controllers/AboutController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Marcusca10.AadAppsDemos.WebApi.AspNetCore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marcusca10.AadAppsDemos.WebApi.AspNetCore.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class AboutController : Controller
+    {
+        // GET api/me
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if (User.Claims != null)
+            {
+                var result = new List<AboutViewModel>();
+                foreach (var claim in User.Claims)
+                {
+                    result.Add(new AboutViewModel
+                        {
+                            Type = claim.Type,
+                            Value = claim.Value,
+                            Issuer = claim.Issuer,
+                            OriginalIssuer = claim.OriginalIssuer
+                        });
+                }
+
+                return Json(result);
+            }
+            else
+                return ValidationProblem();
+        }
+    }
+}
---
+++ WebApi/McaAadWebApiAspNetCore/Models/AboutViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marcusca10.AadAppsDemos.WebApi.AspNetCore.Models
+{
+    public class AboutViewModel
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public string Issuer { get; set; }
+        public string OriginalIssuer { get; set; }
+    }
+}
